2ca8a132d2847476beff53df17f233e08d8cfdfe
Fabian-Ome <fabiantabla@hotmail.com>
2021-08-26 18:53:05 -0500

    .

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.set('pkg', pkg)
 
 app.use(morgan('dev'));
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.json({
         name: app.get('pkg').name,
         author: app.get('pkg').author,
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,16 @@
-import {Router} from 'express'
+import { Router } from 'express'
 const router = Router();
 
-router.get('/' , (req,res) => res.json('get products'));
+import * as productsCtrl from '../controllers/products.controller';
+
+router.post('/', productsCtrl.createProduct);
+
+router.get('/', productsCtrl.getProducts);
+
+router.get('/:productId', productsCtrl.getProductById);
+
+router.put('/:productId', productsCtrl.updateProductById);
+
+router.delete('/:productId', productsCtrl.deleteProductById);
 
 export default router;
