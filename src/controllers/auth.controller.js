f747dccaa89c0a1d3f28ec5370ccaef404c8dd30
Fabian-Ome <fabiantabla@hotmail.com>
2021-08-30 22:56:37 -0500

    authorization config

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,21 @@
 import User from '../models/user';
+import jwt from 'jsonwebtoken';
+import config from '../config';
 
-export const singUp = async (req,res) => {
-    res.status(200).json("no me des error puta")
+export const singUp = async (req, res) => {
+    const { username, email, password, roles } = req.body;
+
+    const newUser = new User({
+        username,
+        email,
+        password: await User.encryptPassword(password)
+    })
+
+    const savedUser = await newUser.save();
+
+    const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
+        expiresIn: 86400 //24hrs 
+    })
+    res.json(token);
 }
-export const singIn = async (req,res) => {}
+export const singIn = async (req, res) => { }
