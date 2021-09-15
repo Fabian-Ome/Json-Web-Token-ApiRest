5215201a007f696d9287e436befaf7bc851df65a
Fabian-Ome <fabiantabla@hotmail.com>
2021-09-15 01:23:43 -0500

    Moderator error fixed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -26,6 +26,16 @@ export const singUp = async (req, res) => {
     const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
         expiresIn: 86400 //24hrs 
     })
-    res.status(200).json(token);
+    res.status(200).json({ token });
 }
-export const singIn = async (req, res) => { }
+export const singIn = async (req, res) => {
+    const userFound = await User.findOne({ email: req.body.email }).populate('roles');
+    console.log(userFound)
+
+    if (!userFound) return res.status(400).json({ message: "User not found" })
+
+    await User.comparePassword(req.body.password, userFound.password)
+
+    console.log(userFound)
+    res.json({ token: "" })
+}
